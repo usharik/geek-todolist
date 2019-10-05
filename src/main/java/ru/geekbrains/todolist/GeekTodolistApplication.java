fcd93921638d3c87959019e70afa9dfda6d05f0b
usharik <usharik@mail.ru>
2019-10-05 12:29:10 +0200

    Code for the second webinar (Spring Security)

--- src/main/java/ru/geekbrains/todolist/GeekTodolistApplication.java
+++ src/main/java/ru/geekbrains/todolist/GeekTodolistApplication.java
@@ -2,9 +2,16 @@ package ru.geekbrains.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class GeekTodolistApplication {
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GeekTodolistApplication.class, args);
